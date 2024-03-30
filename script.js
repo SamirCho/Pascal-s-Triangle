8e8c13a6025959f19ab8b92e93883c8e72f0cf50
Samir Chokshi <samirchokshi11@gmail.com>
2024-03-30 06:29:58 +0000

    Finished

---
+++ script.js
@@ -0,0 +1,21 @@
+let num=20
+
+function factorial(x){
+    if(x<=1){
+        return 1
+    }
+    return x*factorial(x-1)
+}
+
+function choose(n,k){
+    return factorial(n)/(factorial(k)*factorial(n-k))
+}
+
+let a=[]
+for (let i = 0; i < num; i++) {
+    a=[]
+    for (let j = 0; j < i+1; j++) {
+        a.push(choose(i,j))
+    }
+    console.log(a)
+}
